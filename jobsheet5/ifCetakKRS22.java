eec10fcf8902bb77d5adcd0c1752874bab0b3644
najwaadha18 <adhanajwa215@gmail.com>
2025-10-01 11:59:54 +0700

    Tugas 1

--- jobsheet5/ifCetakKRS22.java
+++ jobsheet5/ifCetakKRS22.java
@@ -1,4 +1,5 @@
 package jobsheet5;
+
 import java.util.Scanner; 
 public class ifCetakKRS22 {
 
@@ -8,12 +9,10 @@ public class ifCetakKRS22 {
         System.out.println("--- Cetak KRS SIAKAD ---");
         System.out.print("Apakah UKT sudah kunas? (true/false): ");
         boolean uktLunas = sc.nextBoolean();
-
-        if (uktLunas) {
-            System.out.println("Pembayaran UKT terverifikasi");
-            System.out.println("Silahkan cetak KRS dan minta tanda tangan DPA");
-        } else {
-            System.out.println("Registrasi ditolak. Silakan lunasi UKT terlebih dahulu");
-        }
+        
+         String x = (uktLunas) ? "Pembayaran UKT terverivikasi" : "Registrasi ditolak. Silahkan lunasi UKT terlebih dahulu.";
+        System.out.println(x);
+       
     }
 }
+    
