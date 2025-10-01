461f5e0ed3d4454c486fbeb63917ff929a670cc9
najwaadha18 <adhanajwa215@gmail.com>
2025-10-01 12:24:15 +0700

    Tugas 2

---
+++ jobsheet5/aksesWifi.java
@@ -0,0 +1,18 @@
+package jobsheet5;
+import java.util.Scanner;
+public class aksesWifi {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String pengguna;
+        int sks;
+
+        System.out.println("Masukkan pengguna : ");
+        pengguna = sc.nextLine();
+
+        if (pengguna == "dosen") {
+            System.out.println("Akses wifi diberikan");
+            
+        }
+    }
+}
