56e18290573d9f7b2c024a00ef27642d95619cf5
najwaadha18 <adhanajwa215@gmail.com>
2025-10-28 21:15:45 +0700

    Perubahan Square 22

--- jobsheet8/Square22.java
+++ jobsheet8/Square22.java
@@ -4,12 +4,15 @@ public class Square22 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        System.out.print("Masukkan nilai N = ");
-        int N = sc.nextInt();
-        
-        for (int i = 1; i <= N; i++) {
+        System.out.print("Masukkan nilai n = ");
+        int n = sc.nextInt();
+        for (int iOuter= 1; iOuter<= n; iOuter++) {
+            for (int i = 1; i <= n; i++) {
             System.out.print("*");
-        } 
+            }
+            System.out.println();
+        }
+        
     }
         
 }
