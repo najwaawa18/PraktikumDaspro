f9d4c4646c6424b512c68291bf88c0d97dc744ab
najwaadha18 <adhanajwa215@gmail.com>
2025-11-11 17:06:00 +0700

    Search 22

---
+++ jobsheet9/SearchNilai22.java
@@ -0,0 +1,30 @@
+package jobsheet9;
+import java.util.Scanner;
+public class SearchNilai22 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Masukkan banyaknya nilai yang akan diinput : ");
+        int jumlah = sc.nextInt();
+        int[] arrNilai = new int[jumlah];
+        for (int i = 0; i < arrNilai.length; i++) {
+            System.out.print("Masukkan nilai mahasiswa ke-" + (i+1) + " : ");
+            arrNilai[i] = sc.nextInt();
+        }
+        System.out.print("Masukkan nilai yang ingin dicari : ");
+        int key = sc.nextInt();
+        int hasil = -1;
+        for (int i = 0; i < arrNilai.length; i++) {
+            if (arrNilai[i] == key) {
+                hasil = i;
+                break;
+            }
+        }
+        System.out.println();
+        if (hasil != -1) {
+            System.out.println("Nilai " + key + " ketemu, merupakan nilai mahasiswa ke-" + (hasil + 1)); 
+        } else {
+            System.out.println("Nilai yang dicari tidak ditemukan.");
+        }
+
+    }
+}
